b2c86b36ecebc6b1f0077fffc979e9d3542877dc
Sergey Guchok <gck.sergey@gmail.com>
2019-04-16 00:39:49 +0300

    initial commit

---
+++ precache-manifest.24bf5671d7df7516b950c2744f5560ac.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "955d7cc04d286f921d95a4c3396939e8",
+    "url": "/static/media/second-block-background.955d7cc0.png"
+  },
+  {
+    "revision": "4f2b6e82125ca1f15bc7",
+    "url": "/static/css/main.944f666a.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7646b2d14298543d426c65baac93b1a7",
+    "url": "/static/media/skype.7646b2d1.svg"
+  },
+  {
+    "revision": "d6e125a228b20f51f8a1",
+    "url": "/static/js/2.dc5b612a.chunk.js"
+  },
+  {
+    "revision": "01cd99cbf9b532b633a3fb7f1c290c94",
+    "url": "/static/media/logo.01cd99cb.svg"
+  },
+  {
+    "revision": "fc1d4497271a1088433372ed81050e21",
+    "url": "/static/media/article-cover-1.fc1d4497.png"
+  },
+  {
+    "revision": "1ea5794015a5bf0b2e883a03425a9cfa",
+    "url": "/static/media/article-cover-2.1ea57940.png"
+  },
+  {
+    "revision": "dd9b682f50b325a727ace82e6f11b02f",
+    "url": "/static/media/phone-first-block.dd9b682f.png"
+  },
+  {
+    "revision": "c66245cba324858eb48156fe8870c594",
+    "url": "/static/media/down-arrow.c66245cb.svg"
+  },
+  {
+    "revision": "4f2b6e82125ca1f15bc7",
+    "url": "/static/js/main.2da32638.chunk.js"
+  },
+  {
+    "revision": "1d80e152b4bdea9baed1adf24824de95",
+    "url": "/static/media/analysis.1d80e152.svg"
+  },
+  {
+    "revision": "66e5ac2de85b78fbc32483bb6a565f8d",
+    "url": "/static/media/approach.66e5ac2d.svg"
+  },
+  {
+    "revision": "053f7e09c0278ac253fe69ce8fa544eb",
+    "url": "/static/media/ready-product.053f7e09.svg"
+  },
+  {
+    "revision": "40f07721f9fb2aff373f7ef22b22d38b",
+    "url": "/static/media/load-more-articles.40f07721.svg"
+  },
+  {
+    "revision": "7dd2b23d4557dbd2e05d4fb6ea57a44e",
+    "url": "/static/media/telega.7dd2b23d.svg"
+  },
+  {
+    "revision": "84fd97fc33dd082bce582ad820c01378",
+    "url": "/static/media/vk.84fd97fc.svg"
+  },
+  {
+    "revision": "297ff7061d2eba4dadcd340a3b123707",
+    "url": "/static/media/fb.297ff706.svg"
+  },
+  {
+    "revision": "39691755f2190836806b47f50f34150d",
+    "url": "/static/media/inst.39691755.svg"
+  },
+  {
+    "revision": "d6e125a228b20f51f8a1",
+    "url": "/static/css/2.bc89dae6.chunk.css"
+  },
+  {
+    "revision": "63f9fb80fb241ff2bdf2d5b84c5df42d",
+    "url": "/index.html"
+  }
+];
