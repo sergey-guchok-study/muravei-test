2aaf7f33cc0ab1e2952672c7b10d63acea3e0ef5
Sergey Guchok <gck.sergey@gmail.com>
2019-04-20 13:26:42 +0300

    'asd1'

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.688b9e238bb5bbd10283ba721307dbee.js"
+  "/muravei-test/precache-manifest.cf1e31cadb3f63f4d7b2ee567c5c2185.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/muravei-test/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
